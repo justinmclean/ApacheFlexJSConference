d8683d4cdb74db99561da834dcc1a1dd6fc57dc1
Justin Mclean <jmclean@apache.org>
2017-02-03 16:05:51 +1100

    hard code google analytics id as it not being replaced

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,4 +1,4 @@
-var _gaq=[['_setAccount','{{ . }}'],['_trackPageview']];
+var _gaq=[['_setAccount','UA-91372820-1'],['_trackPageview']];
 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 s.parentNode.insertBefore(g,s)}(document,'script'));
